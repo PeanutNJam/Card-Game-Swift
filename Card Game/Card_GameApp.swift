a51d37a3e70854764afc6ad269dfbe42289e3626
Yu Xuan <ngyuxuan1997@gmail.com>
2024-06-17 15:09:13 +0800

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Yu Xuan Ng on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
